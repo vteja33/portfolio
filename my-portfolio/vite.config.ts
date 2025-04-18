f7f3507eaa783c72c3c106500a02a4544c69cf8f
Vamshi Teja Vallala <vteja33@gmail.com>
2025-04-18 16:23:10 -0400

    fix base path

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/my-portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
