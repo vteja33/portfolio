bd273f93adfd304730911c4d061af6bd9ca5783e
Vamshi Teja Vallala <vteja33@gmail.com>
2025-04-19 00:11:55 -0400

    try fix config.ts

--- my-portfolio/vite.config.ts
+++ my-portfolio/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
